10c2d1540f7b4a0cdc18c02e2bf63d0540ffe1fe
Jeremy Sutton <jssutton11@gmail.com>
2013-05-30 01:57:32 -0400

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
-$redis = Redis.new(:host => uri.host, :port => uri.port)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => "2dd76293e8371cbd9bfbe2000371ce2e")
